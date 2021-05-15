7dc6123add15b9b7b1cfbb9cc7fcb268d92218a5
Cole <76749623+Cool-Cole@users.noreply.github.com>
2021-05-14 20:01:16 -0400

    Update chacha.h

--- src/chacha.h
+++ src/chacha.h
@@ -1,5 +1,10 @@
-#ifndef CHACHA_CHACHAQUARTERROUND_H
-#define CHACHA_CHACHAQUARTERROUND_H
+#ifndef CHACHA_H
+#define CHACHA_H
+
+//C++ guard
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include <stdint.h>
 
@@ -14,18 +19,30 @@ typedef struct {
 /*
  * The key must be 32 bytes (or 256 bits)
  * The nonce must be 12 bytes (or 96 bits)
- * When this function is first called the counter must be 1 unless you want to reinitialize the state at certian points in the encryption process
+ * When this function is first called the counter must be 1 unless you want to reinitialize the state to a certain point in the encryption process
  */
 void ChaChaInitialize(chachastate *cipherInfo, const uint8_t *key, const uint8_t *nonce,
                       uint32_t *counter, uint8_t NumRounds);
 
 /*
- * This function takes an initalized ChaCha state, a plaintext and the length of the plaintext.
+ * This function takes an initialized ChaCha state, a plaintext and the length of the plaintext.
  * The plaintext is encrypted in place.
  */
-void ChaChaEncrypt(chachastate *cipherInfo, uint32_t plaintextLen, uint8_t *plaintext);
+void ChaChaEncrypt(chachastate *cipherInfo, uint32_t plainTextLen, uint8_t *plainText);
+
+/*
+ * This function takes an initialized ChaCha state, a ciphertext and the length of the ciphertext.
+ * The plaintext is encrypted in place.
+ */
+void ChaChaDecrypt(chachastate *cipherInfo, uint32_t plainTextLen, uint8_t *cipherText);
 
 // Don't use this, may be replaced with function definition in future version.
 void fullRound(uint32_t state[]);
 
-#endif //CHACHA_CHACHAQUARTERROUND_H
+//C++ guard
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif //CHACHA_H
